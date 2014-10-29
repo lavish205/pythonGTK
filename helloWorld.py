938d0471a0b7081c0e7b091c86a252c23efeaf12
Lavish Aggarwal <lucky.lavish@gmail.com>
2014-10-29 17:28:21 +0530

    hello world

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+
+from gi.repository import Gtk
+
+def close_hello_world(widget):
+    Gtk.main_quit()
+    
+def print_hello_world(widget):
+    print("Hello World")
+
+window = Gtk.Window()
+window.connect("destroy", close_hello_world)
+        
+button = Gtk.Button("Click here")
+button.connect("clicked", print_hello_world)
+window.add(button)
+        
+window.show_all()
+
+Gtk.main()
